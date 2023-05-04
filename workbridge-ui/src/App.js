8f994682624c8b2061a3e42341d3e4f93a99a2ff
Nancy <nancyhany8@gmail.com>
2023-05-04 10:40:23 +0300

    changes in reactjs

--- workbridge-ui/src/App.js
+++ workbridge-ui/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import './App.css';
+// import './index.css';
+//t
+// import { styled } from '@mui/material/styles';
+// import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faCoffee} from "@fortawesome/free-solid-svg-icons"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-6xl underline font-extrabold bg-green-800 text-red-300">HELLO HELLO HELLO , 
+        This is to validate that Tailwind Works!</h1>
+        <Button variant="contained" color="primary">
+        Click Me to validate that mui works!
+      </Button>
+     <FontAwesomeIcon  icon={faCoffee}></FontAwesomeIcon>
+     <span> this coffee is brought to you by fontAwesome</span>
     </div>
+
   );
 }
 
