f623c3d9ce422678f5d4b64ffc97f809be42cb59
Lojain Mostafa <lojain17mostafa@gmail.com>
2023-05-08 11:58:00 +0300

    WORKBRIDGE-51 Developed login page frontend

--- workbridge-ui/src/App.js
+++ workbridge-ui/src/App.js
@@ -7,17 +7,23 @@
 import { Button } from '@mui/material';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faCoffee} from "@fortawesome/free-solid-svg-icons"
+import Login from './Login.js';
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl underline font-extrabold bg-green-800 text-red-300">HELLO HELLO HELLO , 
-        This is to validate that Tailwind Works!</h1>
-        <Button variant="contained" color="primary">
-        Click Me to validate that mui works!
-      </Button>
-     <FontAwesomeIcon  icon={faCoffee}></FontAwesomeIcon>
-     <span> this coffee is brought to you by fontAwesome</span>
-    </div>
+    //  <div>
+    //    <h1 className="text-6xl underline font-extrabold bg-green-800 text-red-300">HELLO HELLO HELLO , 
+    //     This is to validate that Tailwind Works!</h1>
+    //     <Button variant="contained" color="primary">
+    //    Click Me to validate that mui works!
+    //   </Button>
+    //   <FontAwesomeIcon  icon={faCoffee}></FontAwesomeIcon>
+    // <span> this coffee is brought to you by fontAwesome</span>
+    //  </div>
+    <div className='App'>
+      <Login/>
+      </div>
+
+
   );
 }
 
